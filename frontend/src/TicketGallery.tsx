2dd100e654d7d7971b83073ddfef4e5f07c489c7
abnae <ab.naemi@gmail.com>
2023-04-26 19:26:50 +0200

    Backup for CSS changes.

--- frontend/src/TicketGallery.tsx
+++ frontend/src/TicketGallery.tsx
@@ -6,11 +6,24 @@ type Props = {
 }
 export default function TicketGallery(props: Props){
 
-
+    const openTickets: Ticket[] = props.tickets.filter((ticket) => ticket.status === "OPEN")
+    const inProgressTickets: Ticket[] = props.tickets.filter((ticket) => ticket.status === "IN_PROGRESS")
+    const doneTickets: Ticket[] = props.tickets.filter((ticket) => ticket.status === "DONE")
 
     return (
         <div>
-            {props.tickets.map((ticket) => <TicketCard key={ticket.id} ticket={ticket}/>)
+
+            <h4 className="Status">Open Tickets: </h4>
+            {
+                openTickets.map((ticket) => <TicketCard key={ticket.id} ticket={ticket}/>)
+            }
+            <h4 className="Status">In Progress Tickets: </h4>
+            {
+                inProgressTickets.map((ticket) => <TicketCard key={ticket.id} ticket={ticket}/>)
+            }
+            <h4 className="Status">Done Tickets: </h4>
+            {
+                doneTickets.map((ticket) => <TicketCard key={ticket.id} ticket={ticket}/>)
             }
         </div>
 
