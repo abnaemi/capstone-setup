78bd33aca8abfcea93ee3d8d39901b68b20323a5
abnae <ab.naemi@gmail.com>
2023-04-26 23:16:52 +0200

    Styling Mui and Fonts

--- frontend/src/static/NavigationBar.tsx
+++ frontend/src/static/NavigationBar.tsx
@@ -4,9 +4,9 @@ export default function NavigationBar() {
     return (
 
     <div className="Navigationbar">
-        <Link href="/menu">View Tickets  </Link>
-        <Link href="/archives">Archived Tickets   </Link>
-        <Link href="/add">Add Tickets   </Link>
+        <Link href="/menu"> View Tickets | </Link>
+        <Link href="/archives"> Archived Tickets | </Link>
+        <Link href="/add"> Add Tickets </Link>
     </div>
 )
 
