50b273f814de9056b36e374c5ce50fbef0662862
abnae <ab.naemi@gmail.com>
2023-04-26 22:03:12 +0200

    Used integration of other repo as a help to make integrationtests + Cleaned it

--- backend/src/test/java/de/neuefische/backend/ticket/TicketIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/ticket/TicketIntegrationTest.java
@@ -1,9 +1,5 @@
 package de.neuefische.backend.ticket;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.neuefische.backend.comment.Comment;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,11 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -30,16 +22,9 @@ class TicketIntegrationTest {
     @Autowired
     private  TicketRepository ticketRepository;
 
-    @Autowired
-    private ObjectMapper mapper;
-
-    private Ticket dummyTicket;
-
-    private String jsonTicket;
 
 
         Ticket ticketone = new Ticket("1","Tom","Title","content","123","email","customer","999",new ArrayList<>(), TicketStatus.OPEN);
-        Ticket tickettwo = new Ticket("2","Tom2","Title2","content2","1232","email2","customer2","9992",new ArrayList<>(), TicketStatus.OPEN);
 
 
     @DirtiesContext
