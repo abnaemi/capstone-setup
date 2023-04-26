30f50865552af40aaa9d63579129c5a00071dacd
abnae <ab.naemi@gmail.com>
2023-04-26 22:02:34 +0200

    Used integration of other repo as a help to make integrationtests

--- backend/src/test/java/de/neuefische/backend/ticket/TicketIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/ticket/TicketIntegrationTest.java
@@ -38,78 +38,89 @@ class TicketIntegrationTest {
     private String jsonTicket;
 
 
-    @BeforeEach
-    void setUp() throws Exception {
-        Comment comment = new Comment("1", "comment");
-        Comment comment2 = new Comment("1", "comment");
-
-        List <Comment> commentList = new ArrayList<>();
-        commentList.add(comment);
-        commentList.add(comment2);
+        Ticket ticketone = new Ticket("1","Tom","Title","content","123","email","customer","999",new ArrayList<>(), TicketStatus.OPEN);
+        Ticket tickettwo = new Ticket("2","Tom2","Title2","content2","1232","email2","customer2","9992",new ArrayList<>(), TicketStatus.OPEN);
 
 
-        dummyTicket = new Ticket("1","Tom","Title","content","123","email","customer","999",commentList, TicketStatus.OPEN);
-        jsonTicket = mapper.writeValueAsString(dummyTicket);
-    }
-    @Test
     @DirtiesContext
-    void getAllTickets() throws Exception {
-        ticketRepository.save(dummyTicket);
+    @Test
+    void getAll_ReturnEmptyListBcsNoTicketsExist() throws Exception {
         mockMvc.perform(get("/api/tickets"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[" + jsonTicket + "]"));
+                .andExpect(content().json(
+                        """
+                                []
+                                """
+                ));
     }
 
 
     @Test
-    @DirtiesContext
-    void postTicket() throws Exception {
-        String responseJson =
-                mockMvc.perform(
-                                post("/api/tickets")
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .content(jsonTicket))
-                        .andExpect(status().isCreated())
-                        .andExpect(content().json("""
+    void getAll_shouldReturnAllTickets() throws Exception {
+        ticketRepository.save(ticketone);
+
+
+        mockMvc.perform(get("/api/tickets"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                [
                                 {
-                                  "id": "1",
-                                  "name": "Tom",
-                                  "title": "Title",
-                                  "content": "content",
-                                  "phone": "123",
-                                  "email": "email",
-                                  "customer": "customer",
-                                  "number": "999",
-                                  "comment": [
-                                    {
-                                      "id": "1",
-                                      "comment": "comment"
-                                    },
-                                    {
-                                      "id": "1",
-                                      "comment": "comment"
-                                    }
-                                  ],
-                                  "ticketStatus": "OPEN"
-                                }
-                                """))
-                        .andExpect(jsonPath("$.id").isNotEmpty())
-                        .andReturn()
-                        .getResponse()
-                        .getContentAsString();
-
-
-        Ticket expected = new Ticket(
-                dummyTicket.id(),
-                dummyTicket.name(),
-                dummyTicket.title(),
-                dummyTicket.content(),
-                dummyTicket.phone(),
-                dummyTicket.email(),
-                dummyTicket.customer(),
-                dummyTicket.number(),
-                dummyTicket.comment(),
-                dummyTicket.status());
-        assertThat(ticketRepository.findAll()).contains(expected);
+                        "id": "1",
+                        "name": "Tom",
+                        "title": "Title",
+                        "content": "content",
+                        "phone": "123",
+                        "email": "johndoe@email.com",
+                        "customer": "customer",
+                        "number": "999",
+                        "comment": [],
+                        "status": "OPEN"
+                    }
+                    ]
+                    """
+                ));
     }
+
+    @Test
+    @DirtiesContext
+    void addTicket_shouldReturnTicket() throws Exception {
+        mockMvc.perform(post("/api/tickets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "id": "1",
+                        "name": "Tom",
+                        "title": "Title",
+                        "content": "content",
+                        "phone": "123",
+                        "email": "johndoe@email.com",
+                        "customer": "customer",
+                        "number": "999",
+                        "comment": [],
+                        "status": "OPEN"
+                                }
+                                """
+                        ))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                {
+                                "id": "1",
+                        "name": "Tom",
+                        "title": "Title",
+                        "content": "content",
+                        "phone": "123",
+                        "email": "johndoe@email.com",
+                        "customer": "customer",
+                        "number": "999",
+                        "comment": [],
+                        "status": "OPEN"
+                                }
+                                """
+                ));
+    }
+
+
+
 }
