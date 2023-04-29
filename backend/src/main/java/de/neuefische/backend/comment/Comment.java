88130f1cd0153a1a17bbe348bf13a8a9e20b2496
abnae <ab.naemi@gmail.com>
2023-04-30 00:47:10 +0200

    Added Add Comment Function + Button

--- backend/src/main/java/de/neuefische/backend/comment/Comment.java
+++ backend/src/main/java/de/neuefische/backend/comment/Comment.java
@@ -1,10 +1,12 @@
 package de.neuefische.backend.comment;
 
 
+import org.springframework.data.annotation.Id;
 
 public record Comment(
 
 
+        @Id
         String id,
         String comment
 ) {
--- frontend/src/TicketDetails.tsx
+++ frontend/src/TicketDetails.tsx
@@ -1,61 +1,88 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
-import {toast} from "react-toastify";
-import {Typography} from "@mui/material";
-import {Ticket} from "./model/Ticket";
-
+import { toast } from "react-toastify";
+import { Typography, TextField, Button } from "@mui/material";
+import { Ticket } from "./model/Ticket";
 
 export default function TicketDetail() {
+    const [ticket, setTicket] = useState<Ticket>();
+    const [commentText, setCommentText] = useState("");
 
-    const [ticket, setTicket] = useState<Ticket>()
-
-    const {id} = useParams<{ id: string }>()
+    const { id } = useParams<{ id: string }>();
 
     useEffect(() => {
         if (id) {
-            loadTicketById(id)
+            loadTicketById(id);
         }
-    }, [])
+    }, []);
 
     function loadTicketById(id: string) {
-        axios.get('/api/tickets/' + id)
+        axios
+            .get("/api/tickets/" + id)
             .then((response) => {
-                setTicket(response.data)
+                setTicket(response.data);
             })
             .catch((error) => {
-                toast.error("Ticket not found")
+                toast.error("Ticket not found");
+            });
+    }
+
+    function handleCommentSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        if (!commentText.trim()) {
+            return;
+        }
+        const newComment = { id:"", comment: commentText };
+        const updatedTicket = { ...ticket!, comment: [...ticket!.comment, newComment] };
+        axios
+            .put(`/api/tickets/${ticket?.id}`, updatedTicket)
+            .then((response) => {
+                setTicket(response.data);
+                setCommentText("");
             })
+            .catch((error) => {
+                toast.error("Failed to add comment");
+            });
     }
 
     return (
         <div>
             {ticket ? (
                 <div>
-                    <Typography>Id: {ticket.id}</Typography>
-                    <Typography>Title:{ticket.title}</Typography>
-                    <Typography>Name:{ticket.name}</Typography>
-                    <Typography>Content:{ticket.content}</Typography>
-                    <Typography>Phone:{ticket.phone}</Typography>
-                    <Typography>E-Mail:{ticket.email}</Typography>
-                    <Typography>Customer:{ticket.customer}</Typography>
-                    <Typography>Prio: {ticket.prio}</Typography>
+                    <Typography>{ticket.id}</Typography>
+                    <Typography>{ticket.title}</Typography>
+                    <Typography>{ticket.name}</Typography>
+                    <Typography>{ticket.content}</Typography>
+                    <Typography>{ticket.phone}</Typography>
+                    <Typography>{ticket.email}</Typography>
+                    <Typography>{ticket.customer}</Typography>
+                    <Typography>{ticket.prio}</Typography>
 
                     {ticket.comment.map((c) => (
                         <div key={c.id}>
-                            Comments:
                             <Typography>{c.id}</Typography>
                             <Typography>{c.comment}</Typography>
                         </div>
                     ))}
+
+                    <form onSubmit={handleCommentSubmit}>
+                        <TextField
+                            label="Add a comment"
+                            value={commentText}
+                            onChange={(event) => setCommentText(event.target.value)}
+                            fullWidth
+                            margin="normal"
+                            variant="outlined"
+                        />
+                        <Button type="submit" variant="contained">
+                            Add Comment
+                        </Button>
+                    </form>
                 </div>
             ) : (
                 <div>Loading...</div>
             )}
-
-
-
-
         </div>
     );
 }
