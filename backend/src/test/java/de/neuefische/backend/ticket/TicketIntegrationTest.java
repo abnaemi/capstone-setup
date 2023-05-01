3d9f0a36713a65eb1644cd2f0ad8ee1e864d6e13
abnae <ab.naemi@gmail.com>
2023-05-01 13:59:22 +0200

    Fixed Integrationtest with Annotation to Mock User.

--- backend/src/test/java/de/neuefische/backend/ticket/TicketIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/ticket/TicketIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@WithMockUser
 @SpringBootTest
 @AutoConfigureMockMvc
 class TicketIntegrationTest {
